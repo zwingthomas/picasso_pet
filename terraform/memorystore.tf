e249ae712a480e473af180a2ede65a247d534771
Thomas Zwinger <zwingthomas@gmail.com>
2025-06-28 13:46:09 -0500

    infra: try to prevent redis from destroying and recreating

--- terraform/memorystore.tf
+++ terraform/memorystore.tf
@@ -4,5 +4,5 @@ resource "google_redis_instance" "redis" {
   memory_size_gb         = var.redis_memory_size_gb
   region                 = var.region
   alternative_location_id = var.region_secondary
-  authorized_network     = google_compute_network.vpc.id
+  authorized_network     = google_compute_network.vpc.self_link
 }
