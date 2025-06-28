8d2cb16f2c10cb48213417aa4aa45e75179717d6
Thomas Zwinger <zwingthomas@gmail.com>
2025-06-28 10:46:15 -0500

    infra: add metadata to loadbalancers

--- terraform/loadbalancer.tf
+++ terraform/loadbalancer.tf
@@ -4,6 +4,10 @@ resource "google_cloud_run_domain_mapping" "fastapi_domain" {
   location = var.region
   project  = var.project_id
 
+  metadata {
+    namespace = var.project_id
+  }
+
   spec {
     route_name = google_cloud_run_service.fastapi.name
   }
@@ -13,6 +17,10 @@ resource "google_cloud_run_domain_mapping" "flask_domain" {
   name     = "www.your-domain.com"
   location = var.region
   project  = var.project_id
+
+  metadata {
+    namespace = var.project_id
+  }
 
   spec {
     route_name = google_cloud_run_service.flask.name
